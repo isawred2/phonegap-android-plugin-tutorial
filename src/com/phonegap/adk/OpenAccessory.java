03c7f948e17660810b5648b942aa2a8fc62db4a0
Joe Bowser <joe.bowser@nitobi.com>
2011-10-06 15:40:47 -0700

    Step One

---
+++ src/com/phonegap/adk/OpenAccessory.java
@@ -0,0 +1,16 @@
+package com.phonegap.adk;
+
+import org.json.JSONArray;
+
+import com.phonegap.api.Plugin;
+import com.phonegap.api.PluginResult;
+
+public class OpenAccessory extends Plugin {
+
+	@Override
+	public PluginResult execute(String arg0, JSONArray arg1, String arg2) {
+		// TODO Auto-generated method stub
+		return null;
+	}	
+	
+}
